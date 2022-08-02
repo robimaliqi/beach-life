d275cf3ad361f5de12f36c9d1e93b9d7079c20c7
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-08-02 22:28:49 +0100

    Create routes for each page in app.js
    * Import each page function to app.js
    * Add routes tag to app function
    * Add route with path and element atributes for each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./App.css";
-import Home from "./components/Home";
+import { Home } from "./pages/Home";
+import { Results } from "./pages/Results";
+import { Register } from "./pages/Register";
+import { SignIn } from "./pages/SignIn";
+import { Reviews } from "./pages/Reviews";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Results />} />
+      <Route path="/" element={<Register />} />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<Reviews />} />
+    </Routes>
+  );
 }
 
 export default App;
