b2391289cf9065c429cd29f8e7795952a7c18416
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-08-03 00:17:23 +0100

    Create unit test for Header component
    * Create folder for header in components folder
    * Create a passing unit test
    * Remove app.test.js as it had a failing test

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import { Header } from "../components/Header";
+import { Header } from "../components/Header/Header";
 
 export const Register = () => {
   return <Header title="Register" />;
--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,4 +1,4 @@
-import { Header } from "../components/Header";
+import { Header } from "../components/Header/Header";
 
 export const SignIn = () => {
   return <Header title="Sign In" />;
