558ed4b16d3182a6c73434440fef3f82e5563424
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-08-03 00:12:25 +0100

    Update class name of div in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="main-container">
+    <div className="container">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
