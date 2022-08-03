d3fc5815765acae1dc4c59872b6a26c0bfcfb8e8
Robi <robi.maliqi@hotmail.co.uk>
2022-08-03 21:52:24 +0100

    Add jsx for name entry
    * Add a form where the user can
      enter their user name.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,19 @@
 import { Header } from "../components/Header/Header";
 
 export const Register = () => {
-  return <Header title="Register" />;
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header title="Register" />
+        </div>
+        <form className="form-wrapper">
+          <div className="name">
+            <label clasName="label">Full Name</label>
+            <input className="input" type="text" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
