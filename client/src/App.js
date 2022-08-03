ec6826cd8ef110be90b5341e492c96940c84d3ca
Joe McGarry <joewmcgarry@gmail.com>
2022-08-03 11:42:04 +0100

    Adds first version of map to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./components/Home";
+import Map from "./components/Map";
 
 function App() {
-  return <Home />;
+  return <Map />;
 }
 
 export default App;
