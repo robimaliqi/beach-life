7b032d8f2bce46208aa4b6bbe7cdea3b8cd77330
Hafiz <hafizalimi77@gmail.com>
2022-08-03 16:57:57 +0100

    Adding features to sign up form
    *shows error when you try to submit an empty form
    *can now type in email and password inside input
    *shows success message when email and password filled in and submitted

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -28,35 +28,92 @@
 
 import { useState } from 'react';
 
-
 export function SignIn() {
 
 // States for registration
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
+// States for checking the errors
+const [submitted, setSubmitted] = useState(false);
+const [error, setError] = useState(false);
+
+
+// Handling the email change
+const handleEmail = (e) => {
+	setEmail(e.target.value);
+	setSubmitted(false);
+};
+
+// Handling the password change
+const handlePassword = (e) => {
+	setPassword(e.target.value);
+	setSubmitted(false);
+};
+
+// Handling the form submission
+const handleSubmit = (e) => {
+	e.preventDefault();
+	if (email === '' || password === '') {
+	setError(true);
+	} else {
+	setSubmitted(true);
+	setError(false);
+	}
+};
+
+// Showing success message
+const successMessage = () => {
+	return (
+	<div
+		className="success"
+		style={{
+		display: submitted ? '' : 'none',
+		}}>
+		<h1>User {email} successfully registered!!</h1>
+	</div>
+	);
+};
+
+// Showing error message if error is true
+const errorMessage = () => {
+	return (
+	<div
+		className="error"
+		style={{
+		display: error ? '' : 'none',
+		}}>
+		<h1>Please enter all the fields</h1>
+	</div>
+	);
+};
 
 return (
-  <div className="form">
-    <div>
-      <h1>Sign In</h1>
-    </div>
+	<div className="form">
+	<div>
+		<h1>User Registration</h1>
+	</div>
 
- 
-    <form>
-      {/* Labels and inputs for form data */}
-      <label className="label">Email</label>
-      <input className="input"
-        value={email} type="email" />
+	{/* Calling to the methods */}
+	<div className="messages">
+		{errorMessage()}
+		{successMessage()}
+	</div>
 
-      <label className="label">Password</label>
-      <input className="input"
-        value={password} type="password" />
+	<form>
+		{/* Labels and inputs for form data */}
+		<label className="label">Email</label>
+		<input onChange={handleEmail} className="input"
+		value={email} type="email" />
 
-      <button className="btn" type="submit">
-        Submit
-      </button>
-    </form>
-  </div>
+		<label className="label">Password</label>
+		<input onChange={handlePassword} className="input"
+		value={password} type="password" />
+
+		<button onClick={handleSubmit} className="btn" type="submit">
+		Submit
+		</button>
+	</form>
+	</div>
 );
 }
