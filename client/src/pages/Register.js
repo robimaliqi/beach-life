b747787bccad68abfed9a63fc96298b414e34c71
Robi <robi.maliqi@hotmail.co.uk>
2022-08-03 15:15:04 +0100

    Add regex for validation
    * USER_REGEX; must start with a lowercase or
      uppercase letter, after that it must be
      followed by 3 or to 23 characters that can be
      lowercase or uppercase letters, digits, hyphens
      or underscores. So overall it must be 4 to 24
      characters.
    * PWD_REGEX; requires at least one lower case
      letter, one uppercase letter, one digit and
      one special character. And it can be anywhere
      from 8 to 24 chracters.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,9 @@
 import { Header } from "../components/Header/Header";
 import { useRef, useState, useEffect } from "react";
 
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 export const Register = () => {
   const userRef = useRef();
   const errorRef = userRef();
