341686d5f46b20f36f1ecd366c13b6be9e3df17a
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-03 14:43:46 +0100

    prepared to merge in main

--- server/routes/home.js
+++ server/routes/home.js
@@ -7,4 +7,21 @@ router.get("/", (req, res) => {
   });
 });
 
+// router.get("/beaches", (req, res) => {
+//   async function run() {
+//     try {
+//       const db = client.db("beach_life");
+//       const beaches = db.collection("beaches");
+  
+//       const query = { name: "Shoreham Beach" };
+//       const beach = await beaches.findOne(query);
+//       console.log(beach);
+//     } finally {
+//       await client.close();
+//     }
+//   }
+  
+//   run().catch(console.dir);
+// })
+
 module.exports = router;
