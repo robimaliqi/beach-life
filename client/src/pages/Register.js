44ec35212bc9f8427ddf2e46cc907b7784d961f8
Robi <robi.maliqi@hotmail.co.uk>
2022-08-03 22:11:11 +0100

    Add userState and handleChange
    * Add user state hooks and assgin name, email,
      password to an empty string.
    * To keep track of it's value we assigned
      onChange to handleChnage and with in that
      function it will set the values for each input
      field. First it will take all the initial
      values of each input field and then assign the
      value typed by the user to each input field
      based on it's name.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,18 @@
 import { Header } from "../components/Header/Header";
+import React, { useState } from "react";
+
+const [values, setValues] = useState({
+  fullname: "",
+  email: "",
+  password: "",
+});
+
+const handleChange = (event) => {
+  setValues({
+    ...values,
+    [event.target.name]: event.target.value,
+  });
+};
 
 const handleFormSubmit = (event) => {
   event.preventDefault();
@@ -14,15 +28,33 @@ export const Register = () => {
         <form className="form-wrapper">
           <div className="name">
             <label clasName="label">Full Name</label>
-            <input className="input" type="text" />
+            <input
+              className="input"
+              type="text"
+              name="fullname"
+              value={values.fullname}
+              onChange={handleChange}
+            />
           </div>
           <div className="email">
             <label clasName="label">Email</label>
-            <input className="input" type="email" />
+            <input
+              className="input"
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="password">
             <label clasName="label">Password</label>
-            <input className="input" type="password" />
+            <input
+              className="input"
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <button className="submit" onClick={handleFormSubmit}>
