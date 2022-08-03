2a2d0701fa3db6c88e3acf449cf50d0facefe680
Hafiz <hafizalimi77@gmail.com>
2022-08-03 16:44:54 +0100

    Set up Sign Up form
    *  basic sign up form
       that only requires email and password

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,5 +1,62 @@
-import { Header } from "../components/Header/Header";
+// import { Header } from "../components/Header/Header";
+// import React from "react"
 
-export const SignIn = () => {
-  return <Header title="Sign In" />;
-};
+
+
+
+
+
+
+// export const SignIn = () => {
+
+//   const [name, setNAme] = useState("GFG");
+
+//   const handleChange = () => ();
+
+
+//   return(
+//     <div>
+//       <Header title="Sign In" />
+//       <form>
+//         <input value={name} onChange={handleChange}/>
+//       </form>
+//       <h1>hello</h1>
+//     </div>
+    
+//   )
+// };
+
+import { useState } from 'react';
+
+
+export function SignIn() {
+
+// States for registration
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+
+return (
+  <div className="form">
+    <div>
+      <h1>Sign In</h1>
+    </div>
+
+ 
+    <form>
+      {/* Labels and inputs for form data */}
+      <label className="label">Email</label>
+      <input className="input"
+        value={email} type="email" />
+
+      <label className="label">Password</label>
+      <input className="input"
+        value={password} type="password" />
+
+      <button className="btn" type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+);
+}
