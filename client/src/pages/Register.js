d1a46568608349e37b37198495e15151ae14446d
Robi <robi.maliqi@hotmail.co.uk>
2022-08-03 15:01:17 +0100

    App refs and state
    * Add two use ref refrences, one to put the
      focus on the user when the component loads,
      and another one for the error message.
    * Add state for the user input, we also have
      validName and setValidname that is tied to
      wether the username validates or not. And
      we have userFocus and setUserFocus to check
      wether the focus is on that input field or not.
      Both booleans set to false by default.
    * Add similar states for password and password
      match fields.
    * Add state for error message and success if we
      successfully register.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,24 @@
 import { Header } from "../components/Header/Header";
+import { useRef, useState, useEffect } from "react";
 
 export const Register = () => {
+  const userRef = useRef();
+  const errorRef = userRef();
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = userState(false);
+  const [validFocus, setUserFocus] = userState(false);
+
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = userState(false);
+  const [passwordFocus, setPasswordFocus] = userState(false);
+
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatch, setValidMatch] = userState(false);
+  const [matchFocus, setMatchFocus] = userState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
   return <Header title="Register" />;
 };
