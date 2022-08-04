bd2264b02e0ba496412634153aade45688c7886d
Robi Maliqi <67949995+robimaliqi@users.noreply.github.com>
2022-08-04 14:16:18 +0100

    Updated spelling mistake

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -18,7 +18,7 @@ export const Register = () => {
     if (!values.email) {
       errors.email = "Email is required";
     } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = "Emailis invalid.";
+      errors.email = "Email is invalid.";
     }
     if (!values.password) {
       errors.password = "Password is required.";
