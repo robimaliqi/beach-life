310084c8cbef71c7c18298b55fd2e537af7d82e9
Joe McGarry <joewmcgarry@gmail.com>
2022-08-05 14:19:59 +0100

    Adds beach name to beach page

--- client/src/pages/Beaches.js
+++ client/src/pages/Beaches.js
@@ -1,10 +1,36 @@
 import { useParams } from "react-router-dom";
+const beaches = require("../components/beachList");
 
 export const Beaches = (props) => {
   const { id } = useParams();
+
+  const findBeachName = (id) => {
+    return beaches.map((beach) => {
+      if (beach._id === id) {
+        return beach.name;
+      }
+    });
+  };
+
+  const getLat = (id) => {
+    return beaches.map((beach) => {
+      if (beach._id === id) {
+        return beach.lat;
+      }
+    });
+  };
+
+  const getLong = (id) => {
+    return beaches.map((beach) => {
+      if (beach._id === id) {
+        return beach.long;
+      }
+    });
+  };
+
   return (
     <div>
-      <div>G'day Shiela, {id}</div>
+      <div>Welcome to {findBeachName(id)}</div>
     </div>
   );
 };
