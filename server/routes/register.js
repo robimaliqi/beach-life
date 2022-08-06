d8fe5d5e004ca70f1d413fc85ca609f42cdbfaa1
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-06 06:14:45 +0100

    adds post route to server for adding a new user

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/user')
 
 router.get("/", (req, res) => {
   res.json({
     name: "Register",
+  });
+});
+
+router.post("/", (req, res) => {
+  const user = new User(req.body);
+  user.save((err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("")
   });
 });
 
