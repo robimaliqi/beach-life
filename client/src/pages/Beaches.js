778744d4999e57d811590d0efb85c7b73de0d590
Joe McGarry <joewmcgarry@gmail.com>
2022-08-08 16:38:09 +0100

    Fixes previous issues with API, refactors lat and long calls

--- client/src/pages/Beaches.js
+++ client/src/pages/Beaches.js
@@ -1,24 +1,39 @@
-import { useParams, useState } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 const tidalAPIKey = require("../tide-api");
-const beaches = require("../components/beachList");
+const beachList = require("../components/beachList");
+const tideData = require("../components/beachApiResult");
 
-const TIDE_ENDPOINT = ``;
+// TODO: Get date now and in +X days to end
+// TODO: Get Lat and Long in variable
+// TODO: Hardcode JSON data to test and format output
 
-//TODO: Get date now and in +X days to end
-//TODO: Get Lat and Long in variable
+const getLong = (id) => {
+  return beachList.map((beach) => {
+    if (beach._id === id) {
+      return beach.long;
+    }
+  });
+};
 
 const getLat = (id) => {
-  return beaches.map((beach) => {
+  return beachList.map((beach) => {
     if (beach._id === id) {
       return beach.lat;
     }
   });
 };
 
-const getLong = (id) => {
-  return beaches.map((beach) => {
+const getBeach = (id) => {
+  return beachList.filter((beach) => {
+    return beach._id === id;
+  });
+};
+
+const findBeachName = (id) => {
+  return beachList.map((beach) => {
     if (beach._id === id) {
-      return beach.long;
+      return beach.name;
     }
   });
 };
@@ -26,12 +41,25 @@ const getLong = (id) => {
 export const Beaches = (props) => {
   const { date } = new Date(Date.now).toLocaleDateString();
   const { id } = useParams();
-  const { lat } = getLat(id);
-  const { long } = getLong(id);
+  const [lat, setLat] = useState(getLat(id));
+  const [long, setLong] = useState(-0.2713284100911);
+  const [beach, setBeach] = useState([
+    {
+      _id: "",
+      name: "",
+      district: "",
+      lat: "",
+      long: "",
+    },
+  ]);
 
-  const getStationID = (id) => {
+  useEffect(() => {
+    setBeach(getBeach(id));
+  }, []);
+
+  useEffect(() => {
     fetch(
-      `https://api.stormglass.io/v2/tide/extremes/point?lat=50.8265866683731&lng=-0.2713284100911&start=2022-08-08&end=2022-08-12`,
+      `https://api.stormglass.io/v2/tide/extremes/point?lat=${beach[0].lat}&lng=${beach[0].long}&start=2022-08-08&end=2022-08-12`,
       {
         headers: {
           Authorization: tidalAPIKey,
@@ -43,20 +71,12 @@ export const Beaches = (props) => {
         console.log(responseData);
       })
       .catch((error) => console.log(error));
-  };
-
-  const findBeachName = (id) => {
-    return beaches.map((beach) => {
-      if (beach._id === id) {
-        return beach.name;
-      }
-    });
-  };
+  }, []);
 
   return (
     <div>
       <h1>Welcome to {findBeachName(id)}</h1>
-      <br>{getStationID(id)}</br>
+      <div>{beach[0].lat}</div>
     </div>
   );
 };
