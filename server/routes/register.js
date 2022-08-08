d86dcbd1d69d750659b762a238c414f4af5266d8
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-08 10:29:00 +0100

    encrypts password

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 router.get("/", (req, res) => {
   res.json({
@@ -8,9 +9,11 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/new", (req, res) => {
+router.post("/new", async (req, res) => {
   console.log(req.body)
   const user = new User(req.body);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   user.save((err) => {
     if (err) {
       throw err;
