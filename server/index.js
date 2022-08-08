ade2fafa3179a88987b456ceb20372e92a2ba1b5
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-08 10:20:01 +0100

    post request to database is successful

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const dotenv = require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: "../.env" });
 const cors = require("cors");
 const port = process.env.PORT || 1111; // If the .env file is not working then the port number will be 9999
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Routers
 const homeRouter = require("./routes/home");
@@ -12,15 +21,12 @@ const registerRouter = require("./routes/register");
 const signinRouter = require("./routes/signin");
 const reviewsRouter = require("./routes/reviews");
 
+
 app.use("/home", homeRouter);
 app.use("/results", resultsRouter);
 app.use("/register", registerRouter);
 app.use("/signin", signinRouter);
 app.use("/reviews", reviewsRouter);
-
-// middleware
-app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send({ express: "Backend connected to React" });
