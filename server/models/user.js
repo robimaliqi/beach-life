8c7b149e94885f4e7e9497941bbe6a9909386859
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-09 07:35:15 +0100

    updates schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
+  firstName: {type: String,
+    require: true,
+  },
   lastName: String,
-  email: String,
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
   password: String,
 });
 
