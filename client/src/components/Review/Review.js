67fc89cd3b675dbb7372f82214909bfa1c59311c
Robi <robi.maliqi@hotmail.co.uk>
2022-08-09 16:17:54 +0100

    Add star ratings
    * add star rating as interactive feature but
      still need to store to database so that it can
      show with the comment.

--- client/src/components/Review/Review.js
+++ client/src/components/Review/Review.js
@@ -1,31 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9",
+};
 
 export function Review(props) {
-  const { register, handleSubmit, reset, formState, formState: { errors, isSubmitSuccessful } } = useForm();
-  const [reviews, setReviews ] = useState({reviews: [{
-    _id: "",
-    user: {
-      firstName: "",
-    },
-    text: "",
-    beachId: "",
-    createdAt: "",
-  }]
-})
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
 
-const fetchReviews = () => {
-  fetch(`/reviews/${props.id}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      setReviews(responseJson);
-    });
-};
+  const handleClick = (value) => {
+    setCurrentValue(value);
+  };
+
+  const handleMouseOver = (newHoverValue) => {
+    setHoverValue(newHoverValue);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm();
+
+  const [reviews, setReviews] = useState({
+    reviews: [
+      {
+        _id: "",
+        user: {
+          firstName: "",
+        },
+        text: "",
+        beachId: "",
+        createdAt: "",
+      },
+    ],
+  });
+
+  const fetchReviews = () => {
+    fetch(`/reviews/${props.id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setReviews(responseJson);
+      });
+  };
 
   const onSubmit = (data) => {
     console.log(data);
@@ -37,11 +69,11 @@ const fetchReviews = () => {
       body: JSON.stringify(data),
     });
     fetchReviews();
-  }
+  };
 
   useEffect(() => {
     fetchReviews();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
@@ -51,10 +83,32 @@ const fetchReviews = () => {
 
   const formatDate = (dateString) => {
     return new Date(dateString).toLocaleDateString();
-  }
+  };
 
   return (
     <div>
+      <div style={styles.stars}>
+        {stars.map((_, index) => {
+          return (
+            <FaStar
+              key={index}
+              size={24}
+              onClick={() => handleClick(index + 1)}
+              onMouseOver={() => handleMouseOver(index + 1)}
+              onMouseLeave={handleMouseLeave}
+              color={
+                (hoverValue || currentValue) > index
+                  ? colors.orange
+                  : colors.grey
+              }
+              style={{
+                marginRight: 10,
+                cursor: "pointer",
+              }}
+            />
+          );
+        })}
+      </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <textarea {...register("text", { required: true })} />
         {errors.text && <span>This field is required</span>}
@@ -72,3 +126,29 @@ const fetchReviews = () => {
     </div>
   );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  stars: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  textarea: {
+    border: "1px solid #a9a9a9",
+    borderRadius: 5,
+    padding: 10,
+    margin: "20px 0",
+    minHeight: 100,
+    width: 300,
+  },
+  button: {
+    border: "1px solid #a9a9a9",
+    borderRadius: 5,
+    width: 300,
+    padding: 10,
+  },
+};
