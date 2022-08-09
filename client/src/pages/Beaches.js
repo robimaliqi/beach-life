2d3d02671b0d927e308d09e1a95d92ea5c542740
Joe McGarry <joewmcgarry@gmail.com>
2022-08-09 08:22:11 +0100

    Adds time for today and in 6 days time

--- client/src/pages/Beaches.js
+++ client/src/pages/Beaches.js
@@ -4,45 +4,20 @@ const tidalAPIKey = require("../tide-api");
 const beachList = require("../components/beachList");
 const tideData = require("../components/beachApiResult");
 
-// TODO: Get date now and in +X days to end
-// TODO: Get Lat and Long in variable
-// TODO: Hardcode JSON data to test and format output
-
-const getLong = (id) => {
-  return beachList.map((beach) => {
-    if (beach._id === id) {
-      return beach.long;
-    }
-  });
-};
-
-const getLat = (id) => {
-  return beachList.map((beach) => {
-    if (beach._id === id) {
-      return beach.lat;
-    }
-  });
-};
-
 const getBeach = (id) => {
   return beachList.filter((beach) => {
     return beach._id === id;
   });
 };
 
-const findBeachName = (id) => {
-  return beachList.map((beach) => {
-    if (beach._id === id) {
-      return beach.name;
-    }
-  });
-};
+// TODO: Get date now and in +X days to end
 
 export const Beaches = (props) => {
-  const { date } = new Date(Date.now).toLocaleDateString();
+  const today = new Date().toISOString().split("T")[0];
+  const endDate = new Date(new Date().setDate(new Date().getDate() + 6))
+    .toISOString()
+    .split("T")[0];
   const { id } = useParams();
-  const [lat, setLat] = useState(getLat(id));
-  const [long, setLong] = useState(-0.2713284100911);
   const [beach, setBeach] = useState([
     {
       _id: "",
@@ -57,25 +32,25 @@ export const Beaches = (props) => {
     setBeach(getBeach(id));
   }, []);
 
-  useEffect(() => {
-    fetch(
-      `https://api.stormglass.io/v2/tide/extremes/point?lat=${beach[0].lat}&lng=${beach[0].long}&start=2022-08-08&end=2022-08-12`,
-      {
-        headers: {
-          Authorization: tidalAPIKey,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log(responseData);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.stormglass.io/v2/tide/extremes/point?lat=${beach[0].lat}&lng=${beach[0].long}&start=${today}&end=${endDate}`,
+  //     {
+  //       headers: {
+  //         Authorization: tidalAPIKey,
+  //       },
+  //     }
+  //   )
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       console.log(responseData);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
 
   return (
     <div>
-      <h1>Welcome to {findBeachName(id)}</h1>
+      <h1>Welcome to {beach[0].name}</h1>
       <div>{beach[0].lat}</div>
     </div>
   );
