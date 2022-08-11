f0a0bdaba5555a699a86287fb60859bd87c20b4d
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-08-11 13:53:12 +0100

    fix beach nav bar

--- client/src/components/BeachesNav/BeachesNav.jsx
+++ client/src/components/BeachesNav/BeachesNav.jsx
@@ -6,7 +6,7 @@ import { Emoji } from "../Emojis/Emojis";
 export const BeachesNav = (props) => {
   return (
     <div className="beaches-container">
-      <div className="logo">
+      <div className="beach-nav-logo">
         BeachFinder
         <Emoji symbol={" 🔎"} label="Magnifying Glass Tilted Right" />
       </div>
@@ -15,28 +15,28 @@ export const BeachesNav = (props) => {
         <div className="line"></div>
         <div className="line"></div>
       </div>
-      <nav className="nav-bar">
-        <ul className="nav-bar-list">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
+      <nav className="beach-nav-bar">
+        <ul className="beach-nav-bar-list">
+          <li className="beach-nav-item">
+            <Link to="/" className="beach-nav-link">
               Home
             </Link>
           </li>
           {props.user && (
-            <li className="nav-item">
-              <LogOut className="nav-link">Log Out</LogOut>
+            <li className="beach-nav-item">
+              <LogOut className="beach-nav-link">Log Out</LogOut>
             </li>
           )}
           {!props.user && (
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">
+            <li className="beach-nav-item">
+              <Link to="/register" className="beach-nav-link">
                 Register
               </Link>
             </li>
           )}
           {!props.user && (
-            <li className="nav-item">
-              <Link to="/signin" className="nav-link">
+            <li className="beach-nav-item">
+              <Link to="/signin" className="beach-nav-link">
                 Log In
               </Link>
             </li>
